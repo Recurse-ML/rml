869872e77f8c186b7370e3ab372c6e0b8b51aca6
Vincenzo Incutti <vincenzo.incutti@gmail.com>
2025-05-21 13:33:55 -0700

    Test that only ref locations that exist are included in bc body

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -20,6 +20,13 @@ def filepath_2(tmp_path):
     return file_path
 
 
+@pytest.fixture(scope="function")
+def filepath_3(tmp_path):
+    file_path = tmp_path / "test_3.py"
+    file_path.write_text("def test_3(): pass")
+    return file_path
+
+
 def test_enrich_bc_markdown_with_source_adds_source_for_bc_and_ref_locations(
     filepath_1, filepath_2
 ):
@@ -109,10 +116,10 @@ def test_enrich_bc_markdown_with_source_returns_none_if_error_occurs_while_readi
     comment = APICommentResponse(
         body=comment_body,
         diff_str="",
-        relative_path=filepath_1.name,
+        relative_path=str(filepath_1),
         line_no=1,
         reference_locations=[
-            SourceLocation(relative_path=filepath_2.name, line_no=1),
+            SourceLocation(relative_path=str(filepath_2), line_no=1),
         ],
     )
 
@@ -121,3 +128,52 @@ def test_enrich_bc_markdown_with_source_returns_none_if_error_occurs_while_readi
     enriched_body = enrich_bc_markdown_with_source(comment)
 
     assert enriched_body is None
+
+
+def test_enrich_bc_markdown_with_source_returns_keeps_only_reference_locations_that_exist(
+    filepath_1, filepath_2, filepath_3
+):
+    comment_body = dedent(f"""
+    This change breaks 1 usages of `test_1` across 1 files
+
+    #Symbol: `test_1`
+    
+    ## Affected locations
+
+    {str(filepath_2)}:1
+    {str(filepath_3)}:1
+    """)
+    comment = APICommentResponse(
+        body=comment_body,
+        diff_str="",
+        relative_path=str(filepath_1),
+        line_no=1,
+        reference_locations=[
+            SourceLocation(relative_path=str(filepath_2), line_no=1),
+            SourceLocation(relative_path=str(filepath_3), line_no=1),
+        ],
+    )
+
+    filepath_3.unlink()  # Will cause a FileNotFoundError
+
+    enriched_body = enrich_bc_markdown_with_source(comment)
+
+    assert (
+        enriched_body
+        == dedent(f"""
+    ```python
+    def test_1(): pass
+    ```
+
+    This change breaks 1 usages of `test_1` across 1 files
+
+    #Symbol: `test_1`
+    
+    ## Affected locations
+
+    {str(filepath_2)}:1
+    ```python
+    def test_2(): pass
+    ```
+    """).lstrip()
+    )
