45e63c3c77be5863e3f554709d1344708e22380f
ab-10 <armins.bagrats@gmail.com>
2025-09-09 15:22:48 +0100

    Fix env var presence check

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 def test_e2e():
-    bug_branch = os.environ["BUG_BRANCH"]
+    bug_branch = os.environ.get("BUG_BRANCH")
     assert bug_branch is not None, "BUG_BRANCH is not set"
     result = subprocess.run(
         [
