b0f8f209655ea8672b33ca240bed990964ec79b5
ab-10 <armins.bagrats@gmail.com>
2025-09-09 21:42:12 +0100

    Run the test inside of test repo

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -1,12 +1,18 @@
 import os
 import subprocess
 import sys
+from pathlib import Path
 
 import pytest
 
 
 @pytest.mark.timeout(300)
 def test_e2e():
+    test_repo = os.environ.get("TEST_REPO")
+    assert test_repo is not None, "TEST_REPO is not set"
+    test_repo = Path(test_repo)
+    assert test_repo.exists(), "TEST_REPO does not exist"
+
     bug_branch = os.environ.get("BUG_BRANCH")
     assert bug_branch is not None, "BUG_BRANCH is not set"
     result = subprocess.run(
@@ -20,6 +26,7 @@ def test_e2e():
         ],
         stdout=sys.stdout,
         text=True,
+        cwd=test_repo,
     )
     assert result.returncode == 0
     # TODO (Armin): verify reported output
