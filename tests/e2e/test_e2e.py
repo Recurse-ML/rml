c0e566bc90736554b0faa2c1fabae207c874a11f
ab-10 <armins.bagrats@gmail.com>
2025-09-09 15:15:55 +0100

    Implement initial e2e test

---
+++ tests/e2e/test_e2e.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+
+def test_e2e():
+    bug_branch = os.environ["BUG_BRANCH"]
+    assert bug_branch is not None, "BUG_BRANCH is not set"
+    result = subprocess.run(
+        [
+            "rml",
+            "-md",
+            "--from",
+            bug_branch,
+            "--to",
+            "main",
+        ],
+        capture_output=True,
+        text=True,
+    )
+    assert result.returncode == 0
+    # TODO (Armin): verify reported output
